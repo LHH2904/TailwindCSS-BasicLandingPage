664d02e56bea093f340a132d7ccb7ee41be64c8f
LHH2904 <102815563+LHH2904@users.noreply.github.com>
2024-06-04 17:55:25 +0700

    Complete landing page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,19 @@
+// show and hide hamburger menu in small screen
+const menu = document.getElementById("menu");
+const ulMenu = document.getElementById("ulMenu");
 
+function menuToggle(){
+    menu.classList.toggle("navbar-show");
+}
+
+// browser resize listener
+window.addEventListener("resize", menuResize);
+
+// resize event if user changing the width
+function menuResize(){
+    // first get the size from the window
+    const window_size = window.innerWidth || document.body.clientWidth;
+    if(window.innerWidth > 640){
+        menu.classList.remove("navbar-show");
+    }
+}
